dc658bc6a76598ef013c1670553a158d884b9b82
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-30 09:31:04 -0700

    worked on rawurl and urlencode

--- url.php
+++ url.php
@@ -1,12 +1,15 @@
 <!DOCTYPE>
 <html>
     <head>
-        <title>Name</title>
+        <title>Url</title>
     </head>
     <body>
 <?php 
-
-
+$web="Google Pakistan";
+$search="Jazeb Akram Onlice Courses & Website";
+// rawurlencode is best for first part of search, urlencode is best for second part
+$Result="http://".rawurlencode($web)."?search=".urlencode($search);
+echo $Result
 ?>
     </body>
 </html>
